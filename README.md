d74a2349e303a8dec74cf7e4e19d9f94f1dc87dd
TifaTsubasa <15922402179@163.com>
2016-04-25 18:52:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SwiftNetWorkFlow
---
+++ SwiftNetWorkFlow/NetworkKit/NetworkKit.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkKit.swift
+//  SwiftNetWorkFlow
+//
+//  Created by TifaTsubasa on 16/4/25.
+//  Copyright © 2016年 Upmer Inc. All rights reserved.
+//
+
+import Foundation
